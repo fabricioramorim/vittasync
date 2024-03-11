fec4f0af6aa4e69a9e2f1cf4e61e2e15a4393e23
Fabricio <fabrioceras@gmail.com>
2024-03-11 08:07:26 -0300

    update units

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function importExcelData(Request $request)
+    {
+        $request->validate([
+            'import_file' => [
+                'required',
+                'file'
+            ],
+        ]);
+
+        Excel::import(new CustomerImport, $request->file('import_file'));
+
+        return redirect()->back()->with('status', 'Imported Successfully');
+    }
 }
