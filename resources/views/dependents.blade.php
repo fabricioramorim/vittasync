5ba4701808c3a92e523bfe814b87a628c4e8379d
Fabricio Roney de Amorim <fabrioceras@gmail.com>
2024-03-12 14:16:52 -0300

    Alter dropdown is_corp

--- resources/views/dependents.blade.php
+++ resources/views/dependents.blade.php
@@ -115,13 +115,13 @@
                     class="mt-4 font-semibold block font-sans text-base antialiased leading-relaxed text-inherit cursor-default">
                     @php
                         if (Auth::user()->vaccine_id == 1) {
-                            if ($unit->count() > 0) {
-                                foreach ($unit->where('is_corp', 0) as $ru) {
-                                    if (Auth::user()->unit_id == $ru->id) {
-                                        echo 'Unidade de vacinação: ' . $ru->city . ', ' . $ru->name;
+                                if ($unit->where('is_corp', 0)->count() > 0) {
+                                    foreach ($unit->where('is_corp', 0) as $ru) {
+                                        if ($rs->unit_id == $ru->id) {
+                                            echo 'Unidade de vacinação: ' . $ru->city . ', ' . $ru->name;
+                                        }
                                     }
                                 }
-                            }
                         }
                     @endphp
                 </p>
