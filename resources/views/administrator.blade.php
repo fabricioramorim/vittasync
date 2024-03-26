67e76a2609d186653c1a16bd1a9f23ff25bd9c7b
Fabricio Roney de Amorim <fabrioceras@gmail.com>
2024-03-26 10:37:55 -0300

    test again 10

--- resources/views/administrator.blade.php
+++ resources/views/administrator.blade.php
@@ -145,6 +145,7 @@ foreach ($dependent as $us) {
                 class="col-span-2 relative flex flex-col text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 shadow-md bg-clip-border rounded-xl ">
                 <div class="p-6 ">
                 {{ implode(' ', $dates) }}
+                {{ implode(' ', $dosesByDate) }}
                     <div class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
                     <dl class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
                         <div class="flex flex-col items-center justify-center">
