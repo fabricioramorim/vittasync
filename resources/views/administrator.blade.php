337b33885c47e20e92ce9eca12e5d43fdd466a9e
Fabricio Roney de Amorim <fabrioceras@gmail.com>
2024-03-26 09:37:00 -0300

    Repair col

--- resources/views/administrator.blade.php
+++ resources/views/administrator.blade.php
@@ -127,8 +127,10 @@ foreach ($dependent as $us) {
                 <!-- Line Chart -->
                 <div class="py-6" id="users-chart"></div>
             </div>
-            <div class="col-span-3 max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-                <div class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
+            <div
+                class="col-span-3 relative flex flex-col text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 shadow-md bg-clip-border rounded-xl ">
+                <div class="p-6 ">
+                    <div class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
                     <dl class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
                         <div class="flex flex-col items-center justify-center">
                             <dt class="mb-2 text-3xl font-extrabold">{{ $userTotalApt }}</dt>
