02bce9905ded42fa30bec6bed493e3459d0a6e2d
Fabricio Roney de Amorim <fabrioceras@gmail.com>
2024-03-26 08:19:16 -0300

    test col-span-2

--- resources/views/administrator.blade.php
+++ resources/views/administrator.blade.php
@@ -80,7 +80,6 @@ foreach ($dependent as $us) {
     </x-slot>
 
     <div class="py-12">
-       
         <div class="grid grid-cols-3 gap-8 max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
             <div
                 class="relative flex flex-col text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 shadow-md bg-clip-border rounded-xl ">
@@ -141,37 +140,13 @@ foreach ($dependent as $us) {
             </div>
             
             <!-- Data Label -->
-            <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+            <div class="col-span-2 max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
             <div class="flex justify-between mb-5">
                 <div>
-                <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white me-1">$12,423</h5>
-                <p class="text-xl font-bold leading-none text-gray-900 dark:text-white me-1">Sales this week</p>
-                </div>
-                <div
-                class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
-                23%
-                <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
-                </svg>
+                <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white me-1">Confirmação por período
                 </div>
             </div>
             <div id="data-labels-chart"></div>
-            <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between mt-5">
-                <div class="flex justify-between items-center pt-5">
-                <!-- Button -->
-                <button
-                    id="dropdownDefaultButton"
-                    data-dropdown-toggle="lastDaysdropdown"
-                    data-dropdown-placement="bottom"
-                    class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-                    type="button">
-                    Last 7 days
-                    <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                    </svg>
-                </button>
-                
-            </div>
             </div>
 
 
